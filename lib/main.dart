604041a224d4c9e036345f3726f88e78d6a49050
rashmi-singh2003 <sajalsatsangi2004@gmail.com>
2024-05-07 16:38:50 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.white,
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/logo.png', // Replace with your image path
+                height: 200,
+              ),
+              SizedBox(height: 20),
+              Text(
+                'ResourceHub',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Explore academic, career, and campus resources.',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey[600],
+                ),
+              ),
+              SizedBox(height: 30),
+              Container(
+                width: 200, // Adjust width as needed
+                decoration: BoxDecoration(
+                  border:
+                      Border.all(color: Colors.black), // Add border property
+                  borderRadius:
+                      BorderRadius.circular(10), // Add borderRadius property
+                  boxShadow: [
+                    BoxShadow(
+                      color: const Color.fromARGB(255, 255, 255, 255)
+                          .withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
+                ),
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.black,
+                  ),
+                  onPressed: () {},
+                  child: Text('TextButton'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
