b539ca7af7a9f51f53fe1d7bf6b4601f13b5f745
Nihal saran das duggirala <92871743+nihalsaran@users.noreply.github.com>
2024-05-10 01:38:32 +0530

    UI updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:seekhobuddy/welcome.dart'; // Import the Welcome screen
+import 'package:seekhobuddy/splash_screen.dart';
+ // Import the Welcome screen
 
 void main() {
   runApp(MyApp());
@@ -9,75 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
+      home: SplashScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/logo.png',
-              height: 200,
-            ),
-            SizedBox(height: 20),
-            Text(
-              'ResourceHub',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Explore academic, career, and campus resources.',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.grey[600],
-              ),
-            ),
-            SizedBox(height: 30),
-            Container(
-              width: 200, // Adjust width as needed
-              decoration: BoxDecoration(
-                color: Colors.white, // Change color to white
-                border: Border.all(color: Colors.black), // Add border property
-                borderRadius:
-                    BorderRadius.circular(10), // Add borderRadius property
-                boxShadow: [
-                  BoxShadow(
-                    color: Color.fromARGB(255, 0, 0, 0).withOpacity(1),
-                    offset: Offset(2, 3), // changes position of shadow
-                  ),
-                ],
-              ),
-              child: TextButton(
-                style: TextButton.styleFrom(
-                  foregroundColor: Colors.black,
-                ),
-                onPressed: () {
-                  // Navigate to Welcome screen when TextButton is clicked
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Welcome()),
-                  );
-                },
-                child: Text(
-                  'TextButton',
-                  style: TextStyle(fontFamily: 'RobotoMono'),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
