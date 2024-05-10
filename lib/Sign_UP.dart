e0dd371540cec0d18b59139618a8f8e68bdc359a
rashmi-singh2003 <sajalsatsangi2004@gmail.com>
2024-05-10 12:39:33 +0530

    update sign up page

--- lib/Sign_UP.dart
+++ lib/Sign_UP.dart
@@ -11,13 +11,51 @@ class SignUpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: SlidingUpPanel(
-        minHeight: 90, // Adjust as needed
-        maxHeight: MediaQuery.of(context).size.height * 0.8, // Adjust as needed
-        panelBuilder: (ScrollController sc) => _buildPanel(sc),
-        body:
-            Center(child: Text("This is the widget behind the sliding panel")),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+                'assets/signupbg.png'), // Replace 'assets/background_image.jpg' with your image path
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  Image.asset(
+                    'assets/Splashlogo-removebg-preview.png', // Replace 'assets/logo.png' with your image path
+                    width: 100,
+                    height: 100,
+                    // Adjust width and height as needed
+                  ),
+                  Center(
+                    child: Text(
+                      'SeekhoBuddy',
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontFamily: 'RobotoMono',
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: SlidingUpPanel(
+                minHeight: 90, // Adjust as needed
+                maxHeight: MediaQuery.of(context).size.height *
+                    0.8, // Adjust as needed
+                panelBuilder: (ScrollController sc) => _buildPanel(sc),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -26,53 +64,54 @@ class SignUpScreen extends StatelessWidget {
     return ListView(
       controller: sc,
       children: [
-      Container(
-        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 140),
-        height: 2, // Set height for the divider
-        color: Colors.grey[300], // Color of the divider
-      ),
-      const SizedBox(height: 0),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 10), // Adjust only the left padding for "Student" text
-            child: const Text(
-              'Student',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'RobotoMono',
-              ),
-            ),
-          ),
-          SizedBox(width: 10),
-          Container(
-            width: 50,
-            decoration: BoxDecoration(
-              color: Colors.purpleAccent,
-              border: Border.all(color: Colors.black),
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: [
-                BoxShadow(
-                  color: Color.fromARGB(255, 0, 0, 0).withOpacity(1),
-                  offset: Offset(2, 3),
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 10, horizontal: 140),
+          height: 2, // Set height for the divider
+          color: Colors.grey[300], // Color of the divider
+        ),
+        const SizedBox(height: 0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 10), // Adjust only the left padding for "Student" text
+              child: const Text(
+                'Student',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'RobotoMono',
                 ),
-              ],
-            ),
-            child: TextButton(
-              onPressed: () {
-                // Add functionality for the button
-              },
-              child: Text(
-                'Hi',
-                style: TextStyle(color: Colors.black),
               ),
             ),
-          ),
-        ],
-      ),
-      SizedBox(height: 30),
+            SizedBox(width: 10),
+            Container(
+              width: 50,
+              decoration: BoxDecoration(
+                color: Colors.purpleAccent,
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color.fromARGB(255, 0, 0, 0).withOpacity(1),
+                    offset: Offset(2, 3),
+                  ),
+                ],
+              ),
+              child: TextButton(
+                onPressed: () {
+                  // Add functionality for the button
+                },
+                child: Text(
+                  'Hi',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: 30),
         Container(
           decoration: BoxDecoration(
             border: Border.all(color: Colors.black),
