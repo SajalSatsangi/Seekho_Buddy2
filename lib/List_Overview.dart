8827af977fb4a679e38dd0569176bbc764ee9358
ansh-prasad <anshprasad@gmail.com>
2024-05-11 11:58:44 +0530

    Modify List Overview Page

--- lib/List_Overview.dart
+++ lib/List_Overview.dart
@@ -18,7 +18,12 @@ class MyApp extends StatelessWidget {
                 padding: const EdgeInsets.only(left: 23.0),
                 child: Text('Academic Courses'),
               ),
-              Icon(Icons.book_rounded),
+              IconButton(
+                icon: Icon(Icons.book_rounded),
+                onPressed: () {
+                  // Add functionality for book icon action
+                },
+              ),
             ],
           ),
           leading: IconButton(
@@ -28,32 +33,41 @@ class MyApp extends StatelessWidget {
             },
           ),
         ),
-        body: ListView(
-          children: [
-            CourseItem(
-              title: 'Photography\nBasics',
-              subtitle: 'Mentorship',
-              progress: 69,
-              imageAsset: 'assets/List_Overview.png.png',
-              imageWidth: 120, // Set the width of the image
-              imageHeight: 160, // Set the height of the image
-            ),
-            CourseItem(
-              title: 'Creative\nCareer',
-              progress: 27,
-              imageAsset: 'assets/List_Overview.png.png',
-              imageWidth: 120, // Set the width of the image
-              imageHeight: 160, // Set the height of the image
-            ),
-            CourseItem(
-              title: 'Visual\nCommunication',
-              subtitle: 'Event Planning',
-              progress: 11,
-              imageAsset: 'assets/List_Overview.png.png',
-              imageWidth: 120, // Set the width of the image
-              imageHeight: 160, // Set the height of the image
-            ),
-          ],
+        body: Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black), // Add a border
+            borderRadius: BorderRadius.circular(0.0), // Optional: Add border radius
+          ),
+          child: ListView(
+            children: [
+              CourseItem(
+                title: 'Photography\nBasics',
+                subtitle: 'Mentorship',
+                progress: 69,
+                imageAsset: 'assets/List_Overview.png.png',
+                imageWidth: 120, // Set the width of the image
+                imageHeight: 160, // Set the height of the image
+                icon: Icons.camera_alt,
+              ),
+              CourseItem(
+                title: 'Creative\nCareer',
+                progress: 27,
+                imageAsset: 'assets/List_Overview.png.png',
+                imageWidth: 120, // Set the width of the image
+                imageHeight: 160, // Set the height of the image
+                icon: Icons.create_new_folder,
+              ),
+              CourseItem(
+                title: 'Visual\nCommunication',
+                subtitle: 'Event Planning',
+                progress: 11,
+                imageAsset: 'assets/List_Overview.png.png',
+                imageWidth: 120, // Set the width of the image
+                imageHeight: 160, // Set the height of the image
+                icon: Icons.file_copy,
+              ),
+            ],
+          ),
         ),
         bottomNavigationBar: BottomAppBar(
           child: Row(
@@ -102,6 +116,7 @@ class CourseItem extends StatelessWidget {
   final String imageAsset;
   final double imageWidth;
   final double imageHeight;
+  final IconData? icon;
 
   CourseItem({
     required this.title,
@@ -110,6 +125,7 @@ class CourseItem extends StatelessWidget {
     required this.imageAsset,
     required this.imageWidth,
     required this.imageHeight,
+    this.icon,
   });
 
   @override
@@ -124,25 +140,47 @@ class CourseItem extends StatelessWidget {
             width: imageWidth, // Set the width of the image
             height: imageHeight, // Set the height of the image
           ),
-          SizedBox(width: 24.0), // Add more spacing between the image and details
+          SizedBox(
+            width: 24.0,
+          ), // Add more spacing between the image and details
           Expanded(
             flex: 7,
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center, // Align content vertically at the center
+              mainAxisAlignment: MainAxisAlignment
+                  .center, // Align content vertically at the center
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text(
                       title,
-                      textAlign: TextAlign.start, // Align text starting from the left
+                      textAlign:
+                          TextAlign.start, // Align text starting from the left
                       style: TextStyle(
                         fontFamily: 'RobotoMono',
                         fontWeight: FontWeight.bold,
                         fontSize: 18.0,
                       ),
                     ),
-                    Icon(Icons.book_rounded, size: 20), // Book icon
+                    if (icon != null)
+                      Container(
+                        width: 40, // Set the width of the container
+                        height: 40, // Set the height of the container
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border.all(color: Colors.black),
+                          borderRadius: BorderRadius.circular(5),
+                          boxShadow: [
+                            BoxShadow(
+                              color:
+                                  Color.fromARGB(235, 0, 0, 0).withOpacity(1),
+                              offset: Offset(2, 3),
+                            ),
+                          ],
+                        ),
+                        child: Icon(icon, size: 20), // Book icon
+                      ),
                   ],
                 ),
                 if (subtitle != null)
