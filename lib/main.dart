a832c09aad0c3dbe6ce289227ddf5bdcb86e33e9
SajalSatsangi <sajalsatsangi2004@gmail.com>
2024-05-18 11:08:23 +0530

    Splash Screen Initiated

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,62 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Color.fromRGBO(16, 16, 16, 1),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/Splashlogo.png',
-                height: 100,
-              ),
-              SizedBox(height: 20),
-              Text(
-                'SEEKHO BUDDY',
-                style: TextStyle(
-                  fontFamily: 'PoetsenOne-Regular',
-                  color: Colors.white,
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
+      home: AnimatedSplashScreen(
+        splash: Scaffold(
+          backgroundColor: const Color.fromRGBO(16, 16, 16, 1),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/Splashlogo.png',
+                  height: 150,
                 ),
-              ),
-            ],
+                const SizedBox(height: 20),
+                const Text(
+                  'SEEKHO BUDDY',
+                  style: TextStyle(
+                    fontFamily: 'PoetsenOne',
+                    color: Colors.white,
+                    fontSize: 45,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
+        nextScreen: const LandingPage(),
+        duration: 3100,
+        splashTransition: SplashTransition.fadeTransition,
+        backgroundColor: const Color.fromRGBO(16, 16, 16, 1),
+      ),
+    );
+  }
+}
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Landing Page'),
+      ),
+      body: const Center(
+        child: Text('Welcome to SeekhoBuddy!'),
       ),
     );
   }
