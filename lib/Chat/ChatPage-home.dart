f76b517032a157e50852923193c9a89d1eea54f3
SajalSatsangi <sajalsatsangi2004@gmail.com>
2024-05-21 13:11:54 +0530

    Chat Template Created

---
+++ lib/Chat/ChatPage-home.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:seekhobuddy/Chat/ChatPage.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ChatPage(),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.grey.shade600,
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
+        unselectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            label: "Chats", // Use label instead of title
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group_work),
+            label: "Channels", // Use label instead of title
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_box),
+            label: "Profile", // Use label instead of title
+          ),
+        ],
+      ),
+    );
+  }
+}
