96b1b8248a81842fd7984e913efe6d2eb5456aae
ansh-prasad <anshprasad@gmail.com>
2024-05-31 20:34:12 +0530

    Link About With Home Page

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:seekhobuddy/AboutUs.dart';
 import 'package:seekhobuddy/AdminScreens/Notices-Admin.dart';
 import 'package:seekhobuddy/Courses.dart';
 import 'package:seekhobuddy/Notification.dart';
@@ -117,7 +118,7 @@ class _HomeState extends State<Home> {
                               Navigator.push(
                                 context,
                                 MaterialPageRoute(
-                                    builder: (context) => NotificationsPage()),
+                                    builder: (context) => about()),
                               );
                             },
                             child: Icon(
