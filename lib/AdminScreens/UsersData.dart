3ab2ed89873d30a8b7961e77483fad458166d8e5
ansh-prasad <anshprasad@gmail.com>
2024-05-31 11:35:46 +0530

    User Page Ui

--- lib/AdminScreens/UsersData.dart
+++ lib/AdminScreens/UsersData.dart
@@ -1,157 +1,79 @@
 import 'package:flutter/material.dart';
-import 'package:seekhobuddy/Chat/chatUsersModel.dart';
-import 'package:seekhobuddy/Chat/conversationList.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: UserDataPage(),
+      theme: ThemeData.dark(),
+    );
+  }
+}
 
 class UserDataPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Usersdata(), // Keep the ChatPage as the body
-      ),
-    );
-  }
-}
-
-class Usersdata extends StatefulWidget {
-  @override
-  _UsersdataState createState() => _UsersdataState();
-}
-
-class _UsersdataState extends State<Usersdata> {
-  List<ChatUsers> chatUsers = [
-    ChatUsers(
-      text: "Jane Russel",
-      secondaryText: "Awesome Setup",
-      image:
-          "https://images.unsplash.com/photo-1567515004624-219c11d31f2e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMDUzMDJ8MHwxfHNlYXJjaHw1MXx8bWFufGVufDF8fHx8MTY2Njc4NzAwOA&ixlib=rb-4.0.3&q=80&w=1080",
-      time: "Now",
-    ),
-    ChatUsers(
-      text: "Glady's Murphy",
-      secondaryText: "That's Great",
-      image:
-          "https://images.unsplash.com/photo-1596305589440-2e180399a760?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMDUzMDJ8MHwxfHNlYXJjaHwxMjJ8fGJsYWNrJTIwd29tYW58ZW58MXx8fHwxNjY2ODY1MDg3&ixlib=rb-4.0.3&q=80&w=1080",
-      time: "Yesterday",
-    ),
-    ChatUsers(
-      text: "Jorge Henry",
-      secondaryText: "Hey where are you?",
-      image:
-          "https://images.unsplash.com/photo-1607990281513-2c110a25bd8c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMDUzMDJ8MHwxfHNlYXJjaHwyNXx8bWFufGVufDF8fHx8MTY2NjYzNjY1Nw&ixlib=rb-4.0.3&q=80&w=1080",
-      time: "31 Mar",
-    ),
-    ChatUsers(
-        text: "Philip Fox",
-        secondaryText: "Busy! Call me in 20 mins",
-        image:
-            "https://images.unsplash.com/photo-1567515004624-219c11d31f2e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMDUzMDJ8MHwxfHNlYXJjaHw1MXx8bWFufGVufDF8fHx8MTY2Njc4NzAwOA&ixlib=rb-4.0.3&q=80&w=1080",
-        time: "28 Mar"),
-    ChatUsers(
-        text: "Debra Hawkins",
-        secondaryText: "Thankyou, It's awesome",
-        image:
-            "https://images.unsplash.com/photo-1567515004624-219c11d31f2e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMDUzMDJ8MHwxfHNlYXJjaHw1MXx8bWFufGVufDF8fHx8MTY2Njc4NzAwOA&ixlib=rb-4.0.3&q=80&w=1080",
-        time: "23 Mar"),
-    ChatUsers(
-        text: "Jacob Pena",
-        secondaryText: "will update you in evening",
-        image:
-            "https://images.unsplash.com/photo-1567515004624-219c11d31f2e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMDUzMDJ8MHwxfHNlYXJjaHw1MXx8bWFufGVufDF8fHx8MTY2Njc4NzAwOA&ixlib=rb-4.0.3&q=80&w=1080",
-        time: "17 Mar"),
-    ChatUsers(
-        text: "Andrey Jones",
-        secondaryText: "Can you please share the file?",
-        image:
-            "https://images.unsplash.com/photo-1567515004624-219c11d31f2e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMDUzMDJ8MHwxfHNlYXJjaHw1MXx8bWFufGVufDF8fHx8MTY2Njc4NzAwOA&ixlib=rb-4.0.3&q=80&w=1080",
-        time: "24 Feb"),
-    ChatUsers(
-        text: "John Wick",
-        secondaryText: "How are you?",
-        image:
-            "https://images.unsplash.com/photo-1567515004624-219c11d31f2e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMDUzMDJ8MHwxfHNlYXJjaHw1MXx8bWFufGVufDF8fHx8MTY2Njc4NzAwOA&ixlib=rb-4.0.3&q=80&w=1080",
-        time: "18 Feb"),
-    // Add more ChatUsers with alternate image URLs here...
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        color: Color(0xFF161616), // Set background color to #161616
-        child: SingleChildScrollView(
-          physics: BouncingScrollPhysics(),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              SafeArea(
-                child: Padding(
-                  padding: EdgeInsets.only(left: 16, right: 16, top: 10),
-                  child: Row(
+      body: Padding(
+        padding: EdgeInsets.only(top: 28.0), // Additional padding from the top
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 16, right: 16, top: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Row(
                     children: [
                       IconButton(
                         icon: Icon(Icons.arrow_back_ios, color: Colors.white),
                         onPressed: () {
-                          Navigator.of(context).pop(); // Navigate back
+                          Navigator.of(context).pop();
                         },
                       ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
+                      SizedBox(width: 10.0),
                       Text(
-                        "Users Data",
+                        "Courses",
                         style: TextStyle(
                           fontSize: 28,
                           fontWeight: FontWeight.bold,
-                          color: Colors.white, // Text color
+                          color: Colors.white,
                         ),
                       ),
                     ],
                   ),
-                ),
+                ],
               ),
-              Padding(
-                padding: EdgeInsets.only(top: 16, left: 16, right: 16),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Search...",
-                    hintStyle: TextStyle(color: Colors.white),
-                    prefixIcon: Icon(
-                      Icons.search,
-                      color: Colors.white,
-                      size: 20,
-                    ),
-                    filled: true,
-                    fillColor: Color(0xFF323232),
-                    contentPadding: EdgeInsets.all(8),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      borderSide: BorderSide(color: Color(0xFF323232)),
-                    ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 14, vertical: 14),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Search...",
+                  hintStyle: TextStyle(color: Colors.white),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.white,
+                    size: 20,
+                  ),
+                  filled: true,
+                  fillColor: Color(0xFF323232),
+                  contentPadding: EdgeInsets.all(8),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(color: Color(0xFF323232)),
                   ),
                 ),
               ),
-              ListView.builder(
-                itemCount: chatUsers.length,
-                shrinkWrap: true,
-                padding: EdgeInsets.only(top: 16),
-                physics: NeverScrollableScrollPhysics(),
-                itemBuilder: (context, index) {
-                  return ConversationList(
-                    name: chatUsers[index].text,
-                    messageText: chatUsers[index].secondaryText,
-                    imageUrl: index.isEven
-                        ? "https://images.unsplash.com/photo-1596305589440-2e180399a760?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMDUzMDJ8MHwxfHNlYXJjaHwxMjJ8fGJsYWNrJTIwd29tYW58ZW58MXx8fHwxNjY2ODY1MDg3&ixlib=rb-4.0.3&q=80&w=1080"
-                        : "https://images.unsplash.com/photo-1607990281513-2c110a25bd8c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMDUzMDJ8MHwxfHNlYXJjaHwyNXx8bWFufGVufDF8fHx8MTY2NjYzNjY1Nw&ixlib=rb-4.0.3&q=80&w=1080",
-                    time: chatUsers[index].time,
-                    isMessageRead: (index == 0 || index == 3) ? true : false,
-                  );
-                },
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
+      backgroundColor: Colors.black87,
     );
   }
 }
