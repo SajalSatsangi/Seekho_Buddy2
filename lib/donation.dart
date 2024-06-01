dd20c4dfb4de9dcaabfd82a898b39cb15eee41c6
SajalSatsangi <sajalsatsangi2004@gmail.com>
2024-06-01 17:02:35 +0530

    Refactor search functionality in Subjects.dart and MaterialSection.dart

--- lib/donation.dart
+++ lib/donation.dart
@@ -8,7 +8,7 @@ class DonationPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(
           'Donation',
-          style: TextStyle(fontWeight: FontWeight.bold , color: Colors.white ),
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
         ),
         leading: IconButton(
           icon: Icon(Icons.arrow_back_ios, color: Colors.white),
@@ -16,7 +16,8 @@ class DonationPage extends StatelessWidget {
             Navigator.pop(context);
           },
         ),
-        backgroundColor: const Color(0xFF000000), // Set the background color to black
+        backgroundColor:
+            const Color(0xFF000000), // Set the background color to black
       ),
       backgroundColor: const Color.fromARGB(255, 0, 0, 0),
       body: SingleChildScrollView(
@@ -26,228 +27,11 @@ class DonationPage extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               Container(
+                height: 200.0,
                 padding: EdgeInsets.all(16.0),
                 decoration: BoxDecoration(
                   color: Color(0xFF323232),
                   borderRadius: BorderRadius.circular(16.0), // Rounded corners
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      'Tell us about you',
-                      style: TextStyle(color: Colors.white, fontSize: 16),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      'You are stepping toward wellbeing, we thank you for your contribution',
-                      style: TextStyle(color: Colors.white, fontSize: 16),
-                    ),
-                    SizedBox(height: 16),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Your Name*',
-                        hintStyle: TextStyle(color: Colors.grey),
-                        filled: true,
-                        fillColor: Colors.black,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(16.0), // Rounded corners
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    SizedBox(height: 16),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Your Course',
-                        hintStyle: TextStyle(color: Colors.grey),
-                        filled: true,
-                        fillColor: Colors.black,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(16.0), // Rounded corners
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    SizedBox(height: 16),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Your Year',
-                        hintStyle: TextStyle(color: Colors.grey),
-                        filled: true,
-                        fillColor: Colors.black,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(16.0), // Rounded corners
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    SizedBox(height: 16),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Your Roll Number',
-                        hintStyle: TextStyle(color: Colors.grey),
-                        filled: true,
-                        fillColor: Colors.black,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(16.0), // Rounded corners
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    SizedBox(height: 16),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Your Branch/Specialization/Stream',
-                        hintStyle: TextStyle(color: Colors.grey),
-                        filled: true,
-                        fillColor: Colors.black,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(16.0), // Rounded corners
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    SizedBox(height: 16),
-                    Text(
-                      'Donation Methods',
-                      style: TextStyle(color: Colors.white, fontSize: 16),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      'Give us a brief description of what you are donating to us so that we can put the resources in an appropriate section. We thank you for your help to your juniors.',
-                      style: TextStyle(color: Colors.white, fontSize: 16),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      'There are two ways you can give us your resources and help:',
-                      style: TextStyle(color: Colors.white, fontSize: 16),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      'Through Google Drive',
-                      style: TextStyle(color: Colors.white, fontSize: 16),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      'You can upload your material on your drive, make it public, and send us your drive link.',
-                      style: TextStyle(color: Colors.white, fontSize: 16),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      'Through Upload',
-                      style: TextStyle(color: Colors.white, fontSize: 16),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      'You can upload your pdfs directly here',
-                      style: TextStyle(color: Colors.white, fontSize: 16),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      'We suggest you to choose the first way if you are having a large amount of data and if you are having a few pdfs.fill the rest of the form otherwise submit it.',
-                      style: TextStyle(color: Colors.white, fontSize: 16),
-                    ),
-                    SizedBox(height: 16),
-                    TextField(
-                      minLines: 5, // Increase the number of lines for the description box
-                      maxLines: null,
-                      decoration: InputDecoration(
-                        hintText: 'Description (tell us about whatever you are donating to us in brief)',
-                        hintStyle: TextStyle(color: Colors.grey),
-                        filled: true,
-                        fillColor: Colors.black,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(16.0), // Rounded corners
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    SizedBox(height: 16),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Drive Link',
-                        hintStyle: TextStyle(color: Colors.grey),
-                        filled: true,
-                        fillColor: Colors.black,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(16.0), // Rounded corners
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    SizedBox(height: 16),
-                    TextButton.icon(
-                      onPressed: () {
-                        // Implement your image upload logic here
-                      },
-                      style: TextButton.styleFrom(
-                        backgroundColor: Color.fromARGB(255, 0, 0, 0),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16.0), // Rounded corners
-                        ),
-                      ),
-                      icon: Icon(Icons.file_upload, color: Colors.white),
-                      label: Text(
-                        'Upload Pdf',
-                        style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
-                      ),
-                    ),
-                    SizedBox(height: 16),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Any other Information or Suggestion',
-                        hintStyle: TextStyle(color: Colors.grey),
-                        filled: true,
-                        fillColor: Colors.black,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(16.0), // Rounded corners
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 26.0),
-                      child: Center(
-                        child: ElevatedButton(
-                          onPressed: () {
-                            // Handle add button press
-                            // You can access the text fields' values here
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Color.fromARGB(255, 0, 0, 0),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(
-                                  16.0), // Rounded corners
-                            ),
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 32.0,
-                                vertical:
-                                    16.0), // Increased padding for a larger button
-                            textStyle:
-                                TextStyle(fontSize: 20), // Increased font size
-                          ),
-                          child: Text(
-                            'Donate Now',
-                            style: TextStyle(
-                              color: Color.fromARGB(255, 255, 255, 255),
-                              fontSize: 15, // Increased font size
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-
-                  ],
                 ),
               ),
             ],
