e522a9d3ee4c86309e68e69e68b196ae2e855a4b
SajalSatsangi <sajalsatsangi2004@gmail.com>
2024-06-03 10:18:26 +0530

    Improve notice popup layout and responsiveness

--- lib/AdminScreens/Noticepage/add_notice_popup.dart
+++ lib/AdminScreens/Noticepage/add_notice_popup.dart
@@ -47,203 +47,221 @@ Future<void> showAddPopup(BuildContext context) async {
   return showDialog(
     context: context,
     builder: (BuildContext context) {
-      return AlertDialog(
-        backgroundColor: Color(0xFF323232),
-        title: Text(
-          'Add Notice',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        content: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: titleController,
-                decoration: InputDecoration(
-                  hintText: 'Title',
-                  hintStyle: TextStyle(color: Colors.grey),
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                  ),
-                ),
-                style: TextStyle(color: Colors.white),
+      return LayoutBuilder(
+        builder: (context, constraints) {
+          return AlertDialog(
+            backgroundColor: Color(0xFF323232),
+            title: Text(
+              'Add Notice',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
               ),
-              SizedBox(height: 16),
-              TextField(
-                controller: descriptionController,
-                decoration: InputDecoration(
-                  hintText: 'Description',
-                  hintStyle: TextStyle(color: Colors.grey),
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                  ),
+            ),
+            content: SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: constraints.maxHeight - 150,
+                  minWidth: constraints.maxWidth - 100,
                 ),
-                style: TextStyle(color: Colors.white),
-                maxLines: 5,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextField(
+                      controller: titleController,
+                      decoration: InputDecoration(
+                        hintText: 'Title',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                      ),
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    SizedBox(height: 16),
+                    TextField(
+                      controller: descriptionController,
+                      decoration: InputDecoration(
+                        hintText: 'Description',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                      ),
+                      style: TextStyle(color: Colors.white),
+                      maxLines: 5,
+                    ),
+                    SizedBox(height: 16),
+                    TextButton.icon(
+                      onPressed: () async {
+                        FilePickerResult? result =
+                            await FilePicker.platform.pickFiles();
+                        if (result != null) {
+                          selectedFile = result.files.first;
+                        }
+                      },
+                      style: TextButton.styleFrom(
+                        backgroundColor:
+                            const Color.fromARGB(255, 255, 255, 255),
+                      ),
+                      icon: Icon(Icons.file_upload),
+                      label: Text(
+                        'Upload Image',
+                        style: TextStyle(
+                            color: const Color.fromARGB(255, 0, 0, 0)),
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    MultiSelectDialogField(
+                      items:
+                          faculties.map((e) => MultiSelectItem(e, e)).toList(),
+                      title: Text("Faculties"),
+                      selectedColor: Colors.blue,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.all(Radius.circular(40)),
+                        border: Border.all(color: Colors.white, width: 2),
+                      ),
+                      buttonText: Text(
+                        "Select faculties",
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                      itemsTextStyle: TextStyle(color: Colors.white),
+                      onConfirm: (results) {
+                        selectedFaculties = results;
+                      },
+                    ),
+                    SizedBox(height: 16),
+                    MultiSelectDialogField(
+                      items: subfaculties
+                          .map((e) => MultiSelectItem(e, e))
+                          .toList(),
+                      title: Text("Branches"),
+                      selectedColor: Colors.blue,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.all(Radius.circular(40)),
+                        border: Border.all(color: Colors.white, width: 2),
+                      ),
+                      buttonText: Text(
+                        "Select Branch",
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                      itemsTextStyle: TextStyle(color: Colors.white),
+                      onConfirm: (results) {
+                        selectedSubfaculties = results;
+                      },
+                    ),
+                    SizedBox(height: 16),
+                    MultiSelectDialogField(
+                      items:
+                          semesters.map((e) => MultiSelectItem(e, e)).toList(),
+                      title: Text("Semesters"),
+                      selectedColor: Colors.blue,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.all(Radius.circular(40)),
+                        border: Border.all(color: Colors.white, width: 2),
+                      ),
+                      buttonText: Text(
+                        "Select Semester",
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                      itemsTextStyle: TextStyle(color: Colors.white),
+                      onConfirm: (results) {
+                        selectedSemesters = results;
+                      },
+                    ),
+                    SizedBox(height: 16),
+                    MultiSelectDialogField(
+                      items: subbranches
+                          .map((e) => MultiSelectItem(e, e))
+                          .toList(),
+                      title: Text("Specilization"),
+                      selectedColor: Colors.blue,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.all(Radius.circular(40)),
+                        border: Border.all(color: Colors.white, width: 2),
+                      ),
+                      buttonText: Text(
+                        "Select Specilization",
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                      itemsTextStyle: TextStyle(color: Colors.white),
+                      onConfirm: (results) {
+                        selectedSubbranches = results;
+                      },
+                    ),
+                  ],
+                ),
               ),
-              SizedBox(height: 16),
-              TextButton.icon(
+            ),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text(
+                  'Cancel',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              ElevatedButton(
                 onPressed: () async {
-                  FilePickerResult? result =
-                      await FilePicker.platform.pickFiles();
-                  if (result != null) {
-                    selectedFile = result.files.first;
+                  if (selectedFile != null) {
+                    TaskSnapshot snapshot = await FirebaseStorage.instance
+                        .ref('notices/${selectedFile!.name}')
+                        .putFile(File(selectedFile!.path!));
+
+                    String fileUrl = await snapshot.ref.getDownloadURL();
+
+                    final data = <String, dynamic>{
+                      'title': titleController.text,
+                      'description': descriptionController.text,
+                      'fileUrl': fileUrl,
+                      'date': DateTime.now().toString(),
+                    };
+
+                    if (selectedFaculties.isNotEmpty) {
+                      data['faculties'] = selectedFaculties;
+                    }
+                    if (selectedSubfaculties.isNotEmpty) {
+                      data['subfaculties'] = selectedSubfaculties;
+                    }
+                    if (selectedSemesters.isNotEmpty) {
+                      data['semesters'] = selectedSemesters;
+                    }
+                    if (selectedSubbranches.isNotEmpty) {
+                      data['subbranches'] = selectedSubbranches;
+                    }
+
+                    FirebaseFirestore firestore = FirebaseFirestore.instance;
+                    int docCount = await firestore
+                        .collection('notices')
+                        .get()
+                        .then((querySnapshot) => querySnapshot.docs.length);
+                    int newDocId = docCount + 1;
+
+                    firestore
+                        .collection('notices')
+                        .doc('Notice ' + newDocId.toString())
+                        .set(data);
+
+                    Navigator.of(context).pop();
                   }
                 },
-                style: TextButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 255, 255, 255)),
+                child: Text(
+                  'Add',
+                  style: TextStyle(color: Colors.black),
                 ),
-                icon: Icon(Icons.file_upload),
-                label: Text(
-                  'Upload Image',
-                  style: TextStyle(color: const Color.fromARGB(255, 0, 0, 0)),
-                ),
-              ),
-              SizedBox(height: 16),
-              MultiSelectDialogField(
-                items: faculties.map((e) => MultiSelectItem(e, e)).toList(),
-                title: Text("Faculties"),
-                selectedColor: Colors.blue,
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.all(Radius.circular(40)),
-                  border: Border.all(color: Colors.white, width: 2),
-                ),
-                buttonText: Text(
-                  "Select faculties",
-                  style: TextStyle(color: Colors.white, fontSize: 16),
-                ),
-                itemsTextStyle: TextStyle(color: Colors.white),
-                onConfirm: (results) {
-                  selectedFaculties = results;
-                },
-              ),
-              SizedBox(height: 16),
-              MultiSelectDialogField(
-                items: subfaculties.map((e) => MultiSelectItem(e, e)).toList(),
-                title: Text("Branches"),
-                selectedColor: Colors.blue,
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.all(Radius.circular(40)),
-                  border: Border.all(color: Colors.white, width: 2),
-                ),
-                buttonText: Text(
-                  "Select Branch",
-                  style: TextStyle(color: Colors.white, fontSize: 16),
-                ),
-                itemsTextStyle: TextStyle(color: Colors.white),
-                onConfirm: (results) {
-                  selectedSubfaculties = results;
-                },
-              ),
-              SizedBox(height: 16),
-              MultiSelectDialogField(
-                items: semesters.map((e) => MultiSelectItem(e, e)).toList(),
-                title: Text("Semesters"),
-                selectedColor: Colors.blue,
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.all(Radius.circular(40)),
-                  border: Border.all(color: Colors.white, width: 2),
-                ),
-                buttonText: Text(
-                  "Select Semester",
-                  style: TextStyle(color: Colors.white, fontSize: 16),
-                ),
-                itemsTextStyle: TextStyle(color: Colors.white),
-                onConfirm: (results) {
-                  selectedSemesters = results;
-                },
-              ),
-              SizedBox(height: 16),
-              MultiSelectDialogField(
-                items: subbranches.map((e) => MultiSelectItem(e, e)).toList(),
-                title: Text("Specilization"),
-                selectedColor: Colors.blue,
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.all(Radius.circular(40)),
-                  border: Border.all(color: Colors.white, width: 2),
-                ),
-                buttonText: Text(
-                  "Select Specilization",
-                  style: TextStyle(color: Colors.white, fontSize: 16),
-                ),
-                itemsTextStyle: TextStyle(color: Colors.white),
-                onConfirm: (results) {
-                  selectedSubbranches = results;
-                },
               ),
             ],
-          ),
-        ),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child: Text(
-              'Cancel',
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () async {
-              if (selectedFile != null) {
-                TaskSnapshot snapshot = await FirebaseStorage.instance
-                    .ref('notices/${selectedFile!.name}')
-                    .putFile(File(selectedFile!.path!));
-
-                String fileUrl = await snapshot.ref.getDownloadURL();
-
-                final data = <String, dynamic>{
-                  'title': titleController.text,
-                  'description': descriptionController.text,
-                  'fileUrl': fileUrl,
-                  'date': DateTime.now().toString(),
-                };
-
-                if (selectedFaculties.isNotEmpty) {
-                  data['faculties'] = selectedFaculties;
-                }
-                if (selectedSubfaculties.isNotEmpty) {
-                  data['subfaculties'] = selectedSubfaculties;
-                }
-                if (selectedSemesters.isNotEmpty) {
-                  data['semesters'] = selectedSemesters;
-                }
-                if (selectedSubbranches.isNotEmpty) {
-                  data['subbranches'] = selectedSubbranches;
-                }
-
-                FirebaseFirestore firestore = FirebaseFirestore.instance;
-                int docCount = await firestore
-                    .collection('notices')
-                    .get()
-                    .then((querySnapshot) => querySnapshot.docs.length);
-                int newDocId = docCount + 1;
-
-                firestore
-                    .collection('notices')
-                    .doc('Notice ' + newDocId.toString())
-                    .set(data);
-
-                Navigator.of(context).pop();
-              }
-            },
-            style: ElevatedButton.styleFrom(
-                backgroundColor: Color.fromARGB(255, 255, 255, 255)),
-            child: Text(
-              'Add',
-              style: TextStyle(color: Colors.black),
-            ),
-          ),
-        ],
+          );
+        },
       );
     },
   );
