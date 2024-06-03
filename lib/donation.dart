3a2803c826d9696d08a9459e06ca7576c174965c
SajalSatsangi <sajalsatsangi2004@gmail.com>
2024-06-03 11:00:17 +0530

    Remove unused donation copy.dart file

--- lib/donation.dart
+++ lib/donation.dart
@@ -1,7 +1,8 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class DonationPage extends StatelessWidget {
+  const DonationPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
