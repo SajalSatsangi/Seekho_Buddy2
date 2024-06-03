0cfee6e20923a7091fe98d0d4d5a61669a9b9479
Nihal saran das duggirala <92871743+nihalsaran@users.noreply.github.com>
2024-06-03 11:43:43 +0530

    modified:   ios/Podfile.lock 	modified:   ios/Runner.xcodeproj/project.pbxproj 	modified:   ios/Runner/AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import OneSignal
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,10 +10,23 @@ import Flutter
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
     
-    if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    // Initialize OneSignal
+    OneSignal.setAppId("1cc7933a-5d5d-470b-88fb-451a842f03bf")
+
+    // Set OneSignal delegate
+    OneSignal.setNotificationOpenedHandler { result in
+    // This will be called whenever a notification is opened
+    if let viewController = self.window?.rootViewController as? FlutterViewController {
+        let channel = FlutterMethodChannel(name: "onesignal_flutter",
+                                           binaryMessenger: viewController.binaryMessenger)
+        channel.invokeMethod("notificationOpened", arguments: result.notification.body)
     }
-    
+}
+
+    OneSignal.promptForPushNotifications(userResponse: { accepted in
+        print("User accepted notifications: \(accepted)")
+    })
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
