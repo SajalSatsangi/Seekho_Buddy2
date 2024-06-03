49f52559e8cc9a51f0ba22e3f15e9d442f612174
SajalSatsangi <sajalsatsangi2004@gmail.com>
2024-06-03 10:58:05 +0530

    Remove unused donation copy.dart file

--- lib/donation.dart
+++ lib/donation.dart
@@ -4,11 +4,18 @@ import 'package:flutter/material.dart';
 class DonationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
           'Donation',
-          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+            fontSize: screenWidth * 0.05, // Responsive font size
+          ),
         ),
         leading: IconButton(
           icon: Icon(Icons.arrow_back_ios, color: Colors.white),
@@ -22,15 +29,17 @@ class DonationPage extends StatelessWidget {
       backgroundColor: const Color.fromARGB(255, 0, 0, 0),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
+          padding: EdgeInsets.all(screenWidth * 0.04), // Responsive padding
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               Container(
-                padding: EdgeInsets.all(16.0),
+                padding:
+                    EdgeInsets.all(screenWidth * 0.04), // Responsive padding
                 decoration: BoxDecoration(
                   color: Color(0xFF323232),
-                  borderRadius: BorderRadius.circular(16.0), // Rounded corners
+                  borderRadius: BorderRadius.circular(
+                      screenWidth * 0.05), // Responsive rounded corners
                 ),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -38,193 +47,134 @@ class DonationPage extends StatelessWidget {
                   children: [
                     Text(
                       'Tell us about you',
-                      style: TextStyle(color: Colors.white, fontSize: 16),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: screenWidth * 0.04, // Responsive font size
+                      ),
                     ),
-                    SizedBox(height: 8),
+                    SizedBox(height: screenHeight * 0.015), // Responsive height
                     Text(
                       'You are stepping toward wellbeing, we thank you for your contribution',
-                      style: TextStyle(color: Colors.white, fontSize: 16),
-                    ),
-                    SizedBox(height: 16),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Your Name*',
-                        hintStyle: TextStyle(color: Colors.grey),
-                        filled: true,
-                        fillColor: Colors.black,
-                        border: OutlineInputBorder(
-                          borderRadius:
-                              BorderRadius.circular(16.0), // Rounded corners
-                          borderSide: BorderSide.none,
-                        ),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: screenWidth * 0.04, // Responsive font size
                       ),
-                      style: TextStyle(color: Colors.white),
                     ),
-                    SizedBox(height: 16),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Your Course*',
-                        hintStyle: TextStyle(color: Colors.grey),
-                        filled: true,
-                        fillColor: Colors.black,
-                        border: OutlineInputBorder(
-                          borderRadius:
-                              BorderRadius.circular(16.0), // Rounded corners
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    SizedBox(height: 16),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Your Year*',
-                        hintStyle: TextStyle(color: Colors.grey),
-                        filled: true,
-                        fillColor: Colors.black,
-                        border: OutlineInputBorder(
-                          borderRadius:
-                              BorderRadius.circular(16.0), // Rounded corners
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    SizedBox(height: 16),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Your Roll Number*',
-                        hintStyle: TextStyle(color: Colors.grey),
-                        filled: true,
-                        fillColor: Colors.black,
-                        border: OutlineInputBorder(
-                          borderRadius:
-                              BorderRadius.circular(16.0), // Rounded corners
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    SizedBox(height: 16),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Your Branch/Specialization/Stream*',
-                        hintStyle: TextStyle(color: Colors.grey),
-                        filled: true,
-                        fillColor: Colors.black,
-                        border: OutlineInputBorder(
-                          borderRadius:
-                              BorderRadius.circular(16.0), // Rounded corners
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    SizedBox(height: 16),
+                    SizedBox(height: screenHeight * 0.02), // Responsive height
+                    buildTextField(
+                        context, 'Your Name*', screenWidth, screenHeight),
+                    SizedBox(height: screenHeight * 0.001), // Responsive height
+                    buildTextField(
+                        context, 'Your Course*', screenWidth, screenHeight),
+                    SizedBox(height: screenHeight * 0.001), // Responsive height
+                    buildTextField(
+                        context, 'Your Year*', screenWidth, screenHeight),
+                    SizedBox(height: screenHeight * 0.001), // Responsive height
+                    buildTextField(context, 'Your Roll Number*', screenWidth,
+                        screenHeight),
+                    SizedBox(height: screenHeight * 0.001), // Responsive height
+                    buildTextField(
+                        context,
+                        'Your Branch/Specialization/Stream*',
+                        screenWidth,
+                        screenHeight),
+                    SizedBox(height: screenHeight * 0.02), // Responsive height
                     Text(
                       'Donation Methods',
-                      style: TextStyle(color: Colors.white, fontSize: 16),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: screenWidth * 0.04, // Responsive font size
+                      ),
                     ),
-                    SizedBox(height: 8),
+                    SizedBox(height: screenHeight * 0.015), // Responsive height
                     Text(
                       'Give us a brief description of what you are donating to us so that we can put the resources in an appropriate section. We thank you for your help to your juniors.',
-                      style: TextStyle(color: Colors.white, fontSize: 16),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: screenWidth * 0.04, // Responsive font size
+                      ),
                     ),
-                    SizedBox(height: 8),
+                    SizedBox(height: screenHeight * 0.015), // Responsive height
                     Text(
                       'There are two ways you can give us your resources and help:',
-                      style: TextStyle(color: Colors.white, fontSize: 16),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: screenWidth * 0.04, // Responsive font size
+                      ),
                     ),
-                    SizedBox(height: 8),
+                    SizedBox(height: screenHeight * 0.015), // Responsive height
                     Text(
                       'Through Google Drive',
-                      style: TextStyle(color: Colors.white, fontSize: 16),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: screenWidth * 0.04, // Responsive font size
+                      ),
                     ),
-                    SizedBox(height: 8),
+                    SizedBox(height: screenHeight * 0.015), // Responsive height
                     Text(
                       'You can upload your material on your drive, make it public, and send us your drive link.',
-                      style: TextStyle(color: Colors.white, fontSize: 16),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: screenWidth * 0.04, // Responsive font size
+                      ),
                     ),
-                    SizedBox(height: 8),
+                    SizedBox(height: screenHeight * 0.015), // Responsive height
                     Text(
                       'Through Upload',
-                      style: TextStyle(color: Colors.white, fontSize: 16),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: screenWidth * 0.04, // Responsive font size
+                      ),
                     ),
-                    SizedBox(height: 8),
+                    SizedBox(height: screenHeight * 0.015), // Responsive height
                     Text(
                       'You can upload your pdfs directly here',
-                      style: TextStyle(color: Colors.white, fontSize: 16),
-                    ),
-                    SizedBox(height: 16),
-                    TextField(
-                      minLines:
-                          5, // Increase the number of lines for the description box
-                      maxLines: null,
-                      decoration: InputDecoration(
-                        hintText:
-                            'Description (tell us about whatever you are donating to us in brief)',
-                        hintStyle: TextStyle(color: Colors.grey),
-                        filled: true,
-                        fillColor: Colors.black,
-                        border: OutlineInputBorder(
-                          borderRadius:
-                              BorderRadius.circular(16.0), // Rounded corners
-                          borderSide: BorderSide.none,
-                        ),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: screenWidth * 0.04, // Responsive font size
                       ),
-                      style: TextStyle(color: Colors.white),
                     ),
-                    SizedBox(height: 16),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Drive Link',
-                        hintStyle: TextStyle(color: Colors.grey),
-                        filled: true,
-                        fillColor: Colors.black,
-                        border: OutlineInputBorder(
-                          borderRadius:
-                              BorderRadius.circular(16.0), // Rounded corners
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                      style: TextStyle(color: Colors.white),
+                    SizedBox(height: screenHeight * 0.03), // Responsive height
+                    buildTextField(
+                      context,
+                      'Description (tell us about whatever you are donating to us in brief)',
+                      screenWidth,
+                      screenHeight,
+                      maxLines: 5,
                     ),
-                    SizedBox(height: 16),
-                    TextButton.icon(
+                    SizedBox(height: screenHeight * 0.001), // Responsive height
+                    buildTextField(
+                        context, 'Drive Link', screenWidth, screenHeight),
+                    SizedBox(height: screenHeight * 0), // Responsive height
+                    ElevatedButton.icon(
                       onPressed: () {
                         // Implement your image upload logic here
                       },
-                      style: TextButton.styleFrom(
+                      style: ElevatedButton.styleFrom(
                         backgroundColor: Color.fromARGB(255, 0, 0, 0),
                         shape: RoundedRectangleBorder(
-                          borderRadius:
-                              BorderRadius.circular(16.0), // Rounded corners
+                          borderRadius: BorderRadius.circular(
+                              screenWidth * 0.05), // Responsive rounded corners
                         ),
                       ),
                       icon: Icon(Icons.file_upload, color: Colors.white),
                       label: Text(
                         'Upload Pdf',
                         style: TextStyle(
-                            color: Color.fromARGB(255, 255, 255, 255)),
-                      ),
-                    ),
-                    SizedBox(height: 16),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Any other Information or Suggestion',
-                        hintStyle: TextStyle(color: Colors.grey),
-                        filled: true,
-                        fillColor: Colors.black,
-                        border: OutlineInputBorder(
-                          borderRadius:
-                              BorderRadius.circular(16.0), // Rounded corners
-                          borderSide: BorderSide.none,
+                          color: Color.fromARGB(255, 255, 255, 255),
+                          fontSize: screenWidth * 0.04, // Responsive font size
                         ),
                       ),
-                      style: TextStyle(color: Colors.white),
                     ),
+                    SizedBox(height: screenHeight * 0.02), // Responsive height
+                    buildTextField(
+                        context,
+                        'Any other Information or Suggestion',
+                        screenWidth,
+                        screenHeight),
                     Padding(
-                      padding: const EdgeInsets.only(top: 26.0),
+                      padding: EdgeInsets.only(
+                          top: screenHeight * 0.04), // Responsive padding
                       child: Center(
                         child: ElevatedButton(
                           onPressed: () {
@@ -234,21 +184,22 @@ class DonationPage extends StatelessWidget {
                           style: ElevatedButton.styleFrom(
                             backgroundColor: Color.fromARGB(255, 0, 0, 0),
                             shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(
-                                  16.0), // Rounded corners
+                              borderRadius: BorderRadius.circular(screenWidth *
+                                  0.05), // Responsive rounded corners
                             ),
                             padding: EdgeInsets.symmetric(
-                                horizontal: 32.0,
-                                vertical:
-                                    16.0), // Increased padding for a larger button
-                            textStyle:
-                                TextStyle(fontSize: 20), // Increased font size
+                              horizontal:
+                                  screenWidth * 0.1, // Responsive padding
+                              vertical:
+                                  screenHeight * 0.02, // Responsive padding
+                            ),
                           ),
                           child: Text(
                             'Donate Now',
                             style: TextStyle(
                               color: Color.fromARGB(255, 255, 255, 255),
-                              fontSize: 15, // Increased font size
+                              fontSize:
+                                  screenWidth * 0.04, // Responsive font size
                             ),
                           ),
                         ),
@@ -259,6 +210,35 @@ class DonationPage extends StatelessWidget {
               ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildTextField(BuildContext context, String hintText,
+      double screenWidth, double screenHeight,
+      {int maxLines = 1}) {
+    return Container(
+      margin: EdgeInsets.only(bottom: screenHeight * 0.02), // Responsive margin
+      child: TextField(
+        maxLines: maxLines,
+        decoration: InputDecoration(
+          hintText: hintText,
+          hintStyle: TextStyle(
+            color: Colors.grey,
+            fontSize: screenWidth * 0.04, // Responsive font size
+          ),
+          filled: true,
+          fillColor: Colors.black,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(
+                screenWidth * 0.05), // Responsive rounded corners
+            borderSide: BorderSide.none,
+          ),
+        ),
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: screenWidth * 0.04, // Responsive font size
         ),
       ),
     );
