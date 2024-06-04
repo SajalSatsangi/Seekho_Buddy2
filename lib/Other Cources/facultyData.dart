38d61723f4e3c7d4d96d0d00279185e3e9496994
SajalSatsangi <sajalsatsangi2004@gmail.com>
2024-06-04 09:38:15 +0530

    Refactor Profile-Admin.dart for improved visibility and navigation

--- lib/Other Cources/facultyData.dart
+++ lib/Other Cources/facultyData.dart
@@ -22,152 +22,154 @@ class Faculties extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
-  return Scaffold(
-    appBar: AppBar(
-      leading: IconButton(
-        icon: Icon(Icons.arrow_back_ios, color: Colors.white),
-        onPressed: () => Navigator.of(context).pop(),
-      ),
-      title: Text(
-        "Faculties",
-        style: TextStyle(
-          fontSize: 28,
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios, color: Colors.white),
+          onPressed: () => Navigator.of(context).pop(),
         ),
+        title: Text(
+          "Faculties",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.black,
       ),
       backgroundColor: Colors.black,
-    ),
-    backgroundColor: Colors.black,
-    body: Padding(
-      padding: EdgeInsets.only(top: 20.0), // Adjust the padding as needed
-      child: FutureBuilder<Map<String, dynamic>>(
-        future: fetchData(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          }
-          if (snapshot.hasError) {
-            return Center(
-              child: Text('Error: ${snapshot.error}',
-                  style: TextStyle(color: Colors.white)),
-            );
-          }
-          final facultiesData = snapshot.data;
-          print(
-              'Data type: ${facultiesData.runtimeType}'); // This will print the type of the data
-          if (facultiesData is Map) {
+      body: Padding(
+        padding: EdgeInsets.only(top: 20.0), // Adjust the padding as needed
+        child: FutureBuilder<Map<String, dynamic>>(
+          future: fetchData(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+            if (snapshot.hasError) {
+              return Center(
+                child: Text('Error: ${snapshot.error}',
+                    style: TextStyle(color: Colors.white)),
+              );
+            }
+            final facultiesData = snapshot.data;
             print(
-                'Data keys: ${facultiesData?.keys}'); // This will print the keys of the data if it's a Map
-          }
-          if (facultiesData == null ||
-              !facultiesData.containsKey('faculties')) {
-            return Center(
-              child: Text('Unexpected data format',
-                  style: TextStyle(color: Colors.white)),
-            );
-          }
-          final faculties = facultiesData['faculties'].values.toList();
-          return ListView.builder(
-            itemCount: faculties.length,
-            itemBuilder: (context, index) {
-              final faculty = faculties[index];
-              final String name = faculty['facultyName'];
-               print('Faculty name: $name'); // This will print the faculty name
-              return Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 50.0, vertical: 8.0),
-                child: GestureDetector(
-                  onTap: () {},
-                  child: Container(
-                    width: MediaQuery.of(context).size.width * 2,
-                    height: MediaQuery.of(context).size.height * 0.15,
-                    decoration: BoxDecoration(
-                      color: Color(0xFF323232),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Row(
-                      children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.horizontal(
-                              left: Radius.circular(20)),
-                          child: Image.asset(
-                            index == 0
-                                ? 'assets/comm.png'
-                                : index == 1
-                                    ? 'assets/edu.png'
-                                    : index == 2
-                                        ? 'assets/eng.png'
-                                        : index == 3
-                                            ? 'assets/art.png'
-                                            : index == 4
-                                                ? 'assets/eng.png'
-                                                : index == 5
-                                                    ? 'assets/sci.png'
-                                                    : index == 6
-                                                        ? 'assets/tec.png'
-                                                        : 'assets/search_result.png',
-                            width: 131,
-                            height: 131,
-                            fit: BoxFit.cover,
+                'Data type: ${facultiesData.runtimeType}'); // This will print the type of the data
+            if (facultiesData is Map) {
+              print(
+                  'Data keys: ${facultiesData?.keys}'); // This will print the keys of the data if it's a Map
+            }
+            if (facultiesData == null ||
+                !facultiesData.containsKey('faculties')) {
+              return Center(
+                child: Text('Unexpected data format',
+                    style: TextStyle(color: Colors.white)),
+              );
+            }
+            final faculties = facultiesData['faculties'].values.toList();
+            return ListView.builder(
+              itemCount: faculties.length,
+              itemBuilder: (context, index) {
+                final faculty = faculties[index];
+                final String name = faculty['facultyName'];
+                print(
+                    'Faculty name: $name'); // This will print the faculty name
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 50.0, vertical: 8.0),
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 2,
+                      height: MediaQuery.of(context).size.height * 0.15,
+                      decoration: BoxDecoration(
+                        color: Color(0xFF323232),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Row(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.horizontal(
+                                left: Radius.circular(20)),
+                            child: Image.asset(
+                              index == 0
+                                  ? 'assets/comm.png'
+                                  : index == 1
+                                      ? 'assets/edu.png'
+                                      : index == 2
+                                          ? 'assets/eng.png'
+                                          : index == 3
+                                              ? 'assets/art.png'
+                                              : index == 4
+                                                  ? 'assets/eng.png'
+                                                  : index == 5
+                                                      ? 'assets/sci.png'
+                                                      : index == 6
+                                                          ? 'assets/tec.png'
+                                                          : 'assets/search_result.png',
+                              width: 131,
+                              height: 131,
+                              fit: BoxFit.cover,
+                            ),
                           ),
-                        ),
-                        SizedBox(width: 20),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              SizedBox(height: 14),
-                              Text(
-                                name,
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              SizedBox(height: 10),
-                              Align(
-                                alignment: Alignment.centerRight,
-                                child: ElevatedButton(
-                                  onPressed: () {
-                                    Navigator.push(
-                                      context,
-                                      SlideRightPageRoute(
-                                        page: Branches(
-                                          facultyName: name,
-                                          facultyData: faculty,
-                                        ),
-                                      ),
-                                    );
-                                  },
-                                  style: ButtonStyle(
-                                    backgroundColor:
-                                        MaterialStateProperty.all<Color>(
-                                            Colors.white),
+                          SizedBox(width: 10),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(height: 5),
+                                Text(
+                                  name,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 15.0,
+                                    fontWeight: FontWeight.bold,
                                   ),
-                                  child: Text(
-                                    'View',
-                                    style: TextStyle(
-                                      color: Colors.black,
+                                ),
+                                SizedBox(height: 5),
+                                Container(
+                                  height: 30.0,
+                                  width: 75.0,
+                                  child: ElevatedButton(
+                                    onPressed: () {
+                                      Navigator.push(
+                                        context,
+                                        SlideRightPageRoute(
+                                          page: Branches(
+                                            facultyName: name,
+                                            facultyData: faculty,
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                    style: ButtonStyle(
+                                      backgroundColor:
+                                          MaterialStateProperty.all<Color>(
+                                              Colors.white),
+                                    ),
+                                    child: Text(
+                                      'View',
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 10.0,
+                                      ),
                                     ),
                                   ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              );
-            },
-          );
-        },
+                );
+              },
+            );
+          },
+        ),
       ),
-    ),
     );
   }
 }
