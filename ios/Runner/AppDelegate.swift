199bd39bbf8337ef2f02f5d769869c8ece10cf66
Nihal saran das duggirala <92871743+nihalsaran@users.noreply.github.com>
2024-06-05 18:14:18 +0530

    refactor: Update file picker in add_notice_popup.dart for custom file types

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Flutter
 import OneSignal
+import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,24 +9,9 @@ import OneSignal
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+     FirebaseApp.configure()
+        return true
     GeneratedPluginRegistrant.register(with: self)
-    
-    // Initialize OneSignal
-    OneSignal.setAppId("1cc7933a-5d5d-470b-88fb-451a842f03bf")
-
-    // Set OneSignal delegate
-    OneSignal.setNotificationOpenedHandler { result in
-    // This will be called whenever a notification is opened
-    if let viewController = self.window?.rootViewController as? FlutterViewController {
-        let channel = FlutterMethodChannel(name: "onesignal_flutter",
-                                           binaryMessenger: viewController.binaryMessenger)
-        channel.invokeMethod("notificationOpened", arguments: result.notification.body)
-    }
-}
-
-    OneSignal.promptForPushNotifications(userResponse: { accepted in
-        print("User accepted notifications: \(accepted)")
-    })
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
